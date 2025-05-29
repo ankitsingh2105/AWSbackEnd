dee2c45b80a21947eb0d7fd320e13c05042a4fcc
Ankit <ankitchauhan21500@gmail.com>
2025-05-29 21:41:52 +0530

    changed port

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, response)=>{
     response.send("We are live");
 })
 
+app.get("/shit", (req, res)=>{
+    res.send("welcome to aws bruh!!");
+})
+
 app.listen(3000, ()=>{
     console.log("port 80");
 })
